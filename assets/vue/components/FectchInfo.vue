ce654f477dbbcd2b13f2d3fb55c332b9405e7b45
Favour Chidera Emeka <favorodera@gmail.com>
2024-05-29 07:45:57 +0100

    security and update

--- assets/vue/components/FectchInfo.vue
+++ assets/vue/components/FectchInfo.vue
@@ -2,15 +2,15 @@
 import axios from 'axios'
 import { onMounted, ref } from 'vue'
 
-const API_KEY = "1B90548B36C0495886A01E6887F46860"
+const API_KEY = import.meta.env.VITE_VERIPHONE_API_KEY
 const API_URL = 'https://api.veriphone.io/v2/verify'
-const MOBILE_NUMBER = ref('+2348024383756')
-let mobileNumberInfo = ref(null)
+const MOBILE_NUMBER = '+2348024383756'
+const mobileNumberInfo = ref(null)
 
 const fetchMobileNumberInfo = async () => {
   try {
-    const response = await axios.get(`${API_URL}?key=${API_KEY}&phone=${MOBILE_NUMBER.value}`)
-    const mobileNumberInfo = response.data
+    const response = await axios.get(`${API_URL}?key=${API_KEY}&phone=${MOBILE_NUMBER}`)
+    mobileNumberInfo.value = response.data
     console.log(mobileNumberInfo)
   } catch (error) {
     console.log(error)
@@ -22,4 +22,8 @@ onMounted(() => {
 })
 </script>
 
-<template>HELLO</template>
+<template>
+  <div v-if="mobileNumberInfo">
+    <p>{{mobileNumberInfo.phone}}</p>
+  </div>
+</template>
